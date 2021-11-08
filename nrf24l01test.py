abe5412d77c9ca514a988071e565b4b56368ecd4
JD Russo <jrusso@udel.edu>
2021-11-07 21:19:32 -0800

    Added nrf comm test code

---
+++ nrf24l01test.py
@@ -0,0 +1,201 @@
+"""Test for nrf24l01 module.  Portable between MicroPython targets."""
+
+import usys
+import ustruct as struct
+import utime
+from machine import Pin, SPI
+from nrf24l01 import NRF24L01
+from micropython import const
+
+
+import time, ds18x20, onewire
+import time
+
+# Slave pause between receiving data and checking for further packets.
+_RX_POLL_DELAY = const(15)
+# Slave pauses an additional _SLAVE_SEND_DELAY ms after receiving data and before
+# transmitting to allow the (remote) master time to get into receive mode. The
+# master may be a slow device. Value tested with Pyboard, ESP32 and ESP8266.
+_SLAVE_SEND_DELAY = const(10)
+
+if usys.platform == "pyboard":
+    cfg = {"spi": 2, "miso": "Y7", "mosi": "Y8", "sck": "Y6", "csn": "Y5", "ce": "Y4"}
+# elif usys.platform == "esp8266":  # Hardware SPI
+#     cfg = {"spi": 1, "miso": 12, "mosi": 13, "sck": 14, "csn": 4, "ce": 5}
+# elif usys.platform == "esp32":  # Software SPI
+    # cfg = {"spi": -1, "miso": 32, "mosi": 33, "sck": 25, "csn": 26, "ce": 27}
+
+
+elif usys.platform == "rp2":  # PI PICO
+    cfg = {"spi": 0, "miso": 4, "mosi": 7, "sck": 6, "csn": 14, "ce": 17}
+
+
+else:
+    raise ValueError("Unsupported platform {}".format(usys.platform))
+
+def to_fahrenheit(temp_c):
+
+    temp_f = 32. + (temp_c * (9/5))
+    return temp_f
+
+# Addresses are in little-endian format. They correspond to big-endian
+# 0xf0f0f0f0e1, 0xf0f0f0f0d2
+pipes = (b"\xe1\xf0\xf0\xf0\xf0", b"\xd2\xf0\xf0\xf0\xf0")
+
+
+# RF_SETUP register
+POWER_0 = const(0x00)  # -18 dBm
+POWER_1 = const(0x02)  # -12 dBm
+POWER_2 = const(0x04)  # -6 dBm
+POWER_3 = const(0x06)  # 0 dBm
+SPEED_1M = const(0x00)
+SPEED_2M = const(0x08)
+SPEED_250K = const(0x20)
+
+def master():
+    csn = Pin(cfg["csn"], mode=Pin.OUT, value=1)
+    ce = Pin(cfg["ce"], mode=Pin.OUT, value=0)
+    if cfg["spi"] == -1:
+        spi = SPI(-1, sck=Pin(cfg["sck"]), mosi=Pin(cfg["mosi"]), miso=Pin(cfg["miso"]))
+        nrf = NRF24L01(spi, csn, ce, payload_size=8)
+    else:
+        nrf = NRF24L01(SPI(cfg["spi"]), csn, ce, payload_size=8)
+        nrf.set_power_speed(POWER_0, SPEED_1M)
+
+    nrf.open_tx_pipe(pipes[0])
+    nrf.open_rx_pipe(1, pipes[1])
+    nrf.start_listening()
+
+    num_needed = 16
+    num_successes = 0
+    num_failures = 0
+    led_state = 0
+
+    print("Connecting to thermometer")
+    ow = onewire.OneWire(Pin(16)) 
+    ds = ds18x20.DS18X20(ow)
+    roms = ds.scan()
+    ds.convert_temp()
+    time.sleep_ms(750)
+    for rom in roms:
+        temp_c = ds.read_temp(rom)
+        temp_f = to_fahrenheit(temp_c)
+        print("Temp is " + str(temp_f) + "F")
+
+
+    print("NRF24L01 master mode, sending %d packets..." % num_needed)
+
+    while num_successes < num_needed and num_failures < num_needed:
+        # stop listening and send packet
+        nrf.stop_listening()
+        millis = utime.ticks_ms()
+        # led_state = max(1, (led_state << 1) & 0x0F)
+        ds.convert_temp()
+        time.sleep_ms(750)
+        for rom in roms:
+            temp_c = ds.read_temp(rom)
+            temp_f = to_fahrenheit(temp_c)
+            print("Temp is " + str(temp_f) + "F")
+
+        led_state = int(temp_f*100)
+
+        print("sending:", millis, led_state)
+        try:
+            nrf.send(struct.pack("ii", millis, led_state))
+        except OSError:
+            pass
+
+        # start listening again
+        nrf.start_listening()
+
+        # wait for response, with 250ms timeout
+        start_time = utime.ticks_ms()
+        timeout = False
+        while not nrf.any() and not timeout:
+            if utime.ticks_diff(utime.ticks_ms(), start_time) > 250:
+                timeout = True
+
+        if timeout:
+            print("failed, response timed out")
+            num_failures += 1
+
+        else:
+            # recv packet
+            (got_millis,) = struct.unpack("i", nrf.recv())
+
+            # print response and round-trip delay
+            print(
+                "got response:",
+                got_millis,
+                "(delay",
+                utime.ticks_diff(utime.ticks_ms(), got_millis),
+                "ms)",
+            )
+            num_successes += 1
+
+        # delay then loop
+        utime.sleep_ms(250)
+
+    print("master finished sending; successes=%d, failures=%d" % (num_successes, num_failures))
+
+
+def slave():
+    csn = Pin(cfg["csn"], mode=Pin.OUT, value=1)
+    ce = Pin(cfg["ce"], mode=Pin.OUT, value=0)
+    if cfg["spi"] == -1:
+        spi = SPI(-1, sck=Pin(cfg["sck"]), mosi=Pin(cfg["mosi"]), miso=Pin(cfg["miso"]))
+        nrf = NRF24L01(spi, csn, ce, payload_size=8)
+    else:
+        nrf = NRF24L01(SPI(cfg["spi"]), csn, ce, payload_size=8)
+        nrf.set_power_speed(POWER_0, SPEED_1M)
+
+    nrf.open_tx_pipe(pipes[1])
+    nrf.open_rx_pipe(1, pipes[0])
+    nrf.start_listening()
+
+    print("NRF24L01 slave mode, waiting for packets... (ctrl-C to stop)")
+
+    while True:
+        if nrf.any():
+            while nrf.any():
+                buf = nrf.recv()
+                millis, led_state = struct.unpack("ii", buf)
+                print("received:", millis, led_state)
+
+                temp = led_state / 100
+                print("Temp is " + str(temp) + "F")
+
+                for led in leds:
+                    if led_state & 1:
+                        led.on()
+                    else:
+                        led.off()
+                    led_state >>= 1
+                utime.sleep_ms(_RX_POLL_DELAY)
+
+            # Give master time to get into receive mode.
+            utime.sleep_ms(_SLAVE_SEND_DELAY)
+            nrf.stop_listening()
+            try:
+                nrf.send(struct.pack("i", millis))
+            except OSError:
+                pass
+            print("sent response")
+            nrf.start_listening()
+
+
+try:
+    import pyb
+
+    leds = [pyb.LED(i + 1) for i in range(4)]
+except:
+    leds = []
+
+print("NRF24L01 test module loaded")
+print("NRF24L01 pinout for test:")
+print("    CE on", cfg["ce"])
+print("    CSN on", cfg["csn"])
+print("    SCK on", cfg["sck"])
+print("    MISO on", cfg["miso"])
+print("    MOSI on", cfg["mosi"])
+print("run nrf24l01test.slave() on slave, then nrf24l01test.master() on master")
