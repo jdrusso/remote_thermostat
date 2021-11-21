5e371a7695d04d9bc4da63999e8138c3d40821b9
JD Russo <jdr9012@gmail.com>
2021-11-20 19:50:51 -0800

    Added scheduler

--- thermostat/py_nrf.py
+++ thermostat/py_nrf.py
@@ -12,7 +12,83 @@ from lib_nrf24 import NRF24
 import time
 import spidev
 
+import datetime
 
+
+# Scheduling: A time range, associated with a temperature range
+
+class Schedule:
+
+    def __init__(self, default_range=[70, 72, 1]):
+
+        self.times = []
+        self.temps = []
+
+        self.add_range(default_range, 'default')
+
+    def get_current_target_temp_range(self, current_time = None):
+        """Obtains the target temperature for the current time. Returns the default if no match is found."""
+
+        current_range = self.default_range
+
+        # You shouldn't pass a time in general, but this lets me test it more easily.
+        if current_time is None:
+            current_time = datetime.datetime.now().time()
+
+        for time_idx, time_range in self.times:
+            if time_range[0] < current_time <= time_range[1]:
+                current_range = self.temps[time_idx]
+                print(f"\t Schedule range is {time_range[0]} - {time_range[1]}")
+                break
+            else:
+                pass
+
+        print(f"\t At time {current_time}, the target temp range is {current_range[0]} - {current_range[1]}.")
+
+        return current_range
+
+    def add_range(self, temp_range, time_range):
+
+        # Allow setting a default range, in case no temp range has been set for the current time
+        if time_range == 'default':
+            self.default_range = temp_range
+            return
+
+        else:
+
+            # If the time interval spans midnight, split it up into two
+            midnight_pm = datetime.time(23, 59)
+            midnight_am = datetime.time(0, 0)
+            # TODO: Best way to handle midnight crossover? I can automatically split a time range that crosses midnight
+            #   but that's a little janky
+            # This is a hell of an if statement
+            if midnight_pm > time_range[0] > time_range[1] > midnight_am:
+
+                # Split the time range [start, end] into [start, midnight] [midnight, end]
+                new_ranges = [
+                    [time_range[0], midnight_pm],
+                    [datetime.time(0,0), time_range[1]]
+                ]
+
+                # And add as two independent schedule entries
+                for time_range in new_ranges:
+                    self.times.append(time_range)
+                    self.temps.append(temp_range)
+
+            else:
+                # Add the new time and temp ranges
+                self.times.append(time_range)
+                self.temps.append(temp_range)
+
+                # Make sure these lists didn't get out of whack somehow
+                assert len(self.times) == len(self.temps)
+
+            # TODO: Check that the current time range doesn't overlap anything existing...
+            # TODO: Validate that time_range and temp_range are legitimate.
+            #   Time_range should both be times.
+            #   Temp_range should have valid temps, and a valid hysteresis
+
+            return
 
 class Thermostat:
 
@@ -32,6 +108,8 @@ class Thermostat:
         self.heating = False
         self.cooling = False
         self.fan_is_on = False
+
+        self.schedule = Schedule()
 
     def fan_on(self):
         # self.fan_pin.on()
@@ -72,6 +150,7 @@ class Thermostat:
         self.cooling = False
 
     def set_target_temp_range(self, low_temp, high_temp, hysteresis=1):
+        # This is deprecated now in favor of using a Schedule()
 
         assert low_temp < high_temp, \
             "Invalid temp range -- provide as [low, high]"
@@ -84,6 +163,7 @@ class Thermostat:
 
     def update_state(self, current_temp):
 
+        self.set_target_temp_range(*self.schedule.get_current_target_temp_range())
 
         print("Thermostat state updating -- {%.2f} | [%.2f - %.2f]" % (current_temp, self.low_temp, self.high_temp))
 
@@ -140,7 +220,12 @@ if __name__=="__main__":
                             temp_select_pin=temp_select_pin,
                             fan_pin=fan_pin)
 
-    thermostat.set_target_temp_range(73, 75)
+    # From 8AM to 11PM, set the temp to 71-73 with 1.5 degree hysteresis
+    thermostat.schedule.add_range([71, 73, 1.5], [datetime.time(8, 0), datetime.time(23, 0)])
+
+    # From 11PM to 8AM, set the temp to 62-69 with 1.5 degree hysteresis
+    thermostat.schedule.add_range([62, 69, 1.5], [datetime.time(23, 0), datetime.time(8, 0)])
+
     thermostat.all_off()
 
     # Initialize radio
