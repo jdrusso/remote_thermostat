91cc0e6b1a141f4ae1f6aa483865bd6237f18203
JD Russo <new@email>
2022-02-07 20:53:03 -0800

    Moved display code

---
+++ convenience/display.py
@@ -0,0 +1,105 @@
+import ST7735
+from convenience.fonts import *
+from PIL import Image, ImageDraw
+
+class Colors:
+
+    RED = (225, 6, 0)
+    BLUE = (0, 255, 255)
+    WHITE = (255, 255, 255)
+    YELLOW = (255, 215, 0)
+
+class Display:
+
+    def __init__(self,
+                 port, cs, dc, rst,
+                 rotation, offset_top,
+                 width=128, height=128,
+                 backlight=22,
+                 invert=False,
+                 spi_speed_hz=24000000):
+
+        self.display = ST7735.ST7735(
+            port=port,
+            cs=cs,
+            # cs=ST7735.BG_SPI_CS_FRONT,  # BG_SPI_CSB_BACK or BG_SPI_CS_FRONT
+            dc=dc,  # 27,
+            rst=rst,  # 17,
+            width=width,
+            height=height,
+            backlight=backlight,  # 18 for back BG slot, 19 for front BG slot.
+            rotation=rotation,  # 180,
+            invert=invert,
+            spi_speed_hz=spi_speed_hz,
+            offset_top=offset_top,
+        )
+
+        self.display.begin()
+
+        self.width = width
+        self.height = height
+
+        self.display_initialization_message()
+
+    def refresh_display(self, img):
+
+        self.display.display(img)
+
+    def prepare_new_image(self):
+        """
+        Must be called before drawing, to create a blank canvas.
+        """
+
+        img = Image.new("RGB", (self.width, self.height), color=(0, 0, 0))
+        draw = ImageDraw.Draw(img)
+
+        return img, draw
+
+    def display_initialization_message(self):
+
+        img, draw = self.prepare_new_image()
+        MESSAGE = "INITIALIZING"
+
+        size_x, size_y = draw.textsize(MESSAGE, init_font)
+        draw.text((0, 0), MESSAGE, font=init_font, fill=(255, 255, 255))
+        self.refresh_display(img)
+
+    def update_thermostat_display(self, low_temp, high_temp, local_temp, cur_temp, status, last_update):
+
+        # Update display with
+        #   Target
+        #   Current
+        #   Status
+        #   Last received
+
+        img, draw = self.prepare_new_image()
+
+        # Set messages
+        TGT_MESSAGE = f"[{low_temp} - {high_temp}] | {local_temp:.1f}"
+        CUR_MESSAGE = f"   {cur_temp:.1f}"
+        STATUS_MESSAGE = f"  {status}"
+        LAST_MESSAGE = f"Last: {last_update:%H:%M:%S}"
+
+        # Determine message sizes
+        small_text_width, small_height = draw.textsize(TGT_MESSAGE, small_font)
+        big_text_width, big_height = draw.textsize(TGT_MESSAGE, big_font)
+
+        # Draw baby draw
+        draw.text((0, 0), TGT_MESSAGE, font=small_font, fill=(255, 255, 255))
+        draw.text(
+            (0, small_height * 1.5), CUR_MESSAGE, font=real_big_font, fill=cur_color
+        )
+        draw.text(
+            (0, small_height * 2 + big_height * 1),
+            STATUS_MESSAGE,
+            font=real_big_font,
+            fill=stat_color,
+        )
+        draw.text(
+            (0, small_height * 1 + big_height * 3),
+            LAST_MESSAGE,
+            font=small_font,
+            fill=(255, 255, 255),
+        )
+
+        self.refresh_display(img)
--- pi_zero_thermostat.py
+++ pi_zero_thermostat.py
@@ -4,13 +4,14 @@
 import json
 import datetime
 from PIL import Image, ImageDraw
-import ST7735
 from w1thermsensor import W1ThermSensor
 from rich.console import Console
 
 from convenience.conversions import *
 from convenience.radio import *
 from convenience.fonts import *
+from convenience.display import Display
+from convenience.display import Colors
 from thermostat import Thermostat
 
 import logging
@@ -47,37 +48,16 @@ log = logging.getLogger()
 
 if __name__ == "__main__":
 
-    # Initialize display
-    display = ST7735.ST7735(
+    display = Display(
         port=0,
-        cs=1,
-        # cs=ST7735.BG_SPI_CS_FRONT,  # BG_SPI_CSB_BACK or BG_SPI_CS_FRONT
-        dc=20,  # 27,
-        rst=21,  # 17,
-        width=128,
-        height=128,
-        backlight=22,  # 18 for back BG slot, 19 for front BG slot.
-        rotation=90,  # 180,
-        invert=False,
-        spi_speed_hz=24000000,
-        offset_top=3,
+        cs=1, dc=20, rst=21,
+        rotation=90, offset_top=3,
     )
-    display.begin()
     width = display.width
     height = display.height
 
-    # Display initialization message
-    img = Image.new("RGB", (width, height), color=(0, 0, 0))
-    draw = ImageDraw.Draw(img)
-    MESSAGE = "INITIALIZING"
-    size_x, size_y = draw.textsize(MESSAGE, init_font)
-    draw.text((0, 0), MESSAGE, font=init_font, fill=(255, 255, 255))
-    display.display(img)
-
     # Initialize thermostat
-
     thermostat = Thermostat(temp_control_pin=22, temp_select_pin=18, fan_pin=22)
-
     with open("schedule.json", "r") as schedule_file:
         schedules = json.load(schedule_file)["schedules"]
         for schedule in schedules:
@@ -89,7 +69,6 @@ if __name__ == "__main__":
 
     # Initialize radio
     pipes = [[0xC2, 0xC2, 0xC2, 0xC2, 0xC2], [0xF0, 0xF0, 0xF0, 0xF0, 0xF0]]
-    time.sleep(0.01)
     radio2 = open_radio(
         ce_pin=19,
         csn_pin=0,
@@ -99,8 +78,6 @@ if __name__ == "__main__":
         read_pipe=pipes[0],
         crc=NRF24.CRC_16,
     )
-
-    c = 1
 
     log.info("Entering main loop")
 
@@ -188,58 +165,28 @@ if __name__ == "__main__":
         )
         time.sleep(1.0)
 
-        # Update display with
-        #   Target
-        #   Current
-        #   Status
-        #   Last received
         status = "NONE"
         if thermostat.heating:
             status = "HEAT"
-            stat_color = (225, 6, 0)
+            stat_color = colors.RED
         elif thermostat.cooling:
             status = "COOL"
-            stat_color = (0, 255, 255)
+            stat_color = colors.BLUE
         else:
             status = "OFF"
-            stat_color = (255, 255, 255)
+            stat_color = colors.WHITE
 
         # Make the temperature color yellow if you had to fall back to local temp
         if retries > 2:
-            cur_color = (255, 215, 0)
+            cur_color = colors.YELLOW
         else:
-            cur_color = (255, 255, 255)
+            cur_color = colors.WHITE
 
-        # Prepare the output image
-        img = Image.new("RGB", (width, height), color=(0, 0, 0))
-        draw = ImageDraw.Draw(img)
-
-        # Set messages
-        TGT_MESSAGE = f"[{thermostat.low_temp} - {thermostat.high_temp}] | {calibrated_local_temp:.1f}"
-        CUR_MESSAGE = f"   {cur_temp:.1f}"
-        STATUS_MESSAGE = f"  {status}"
-        LAST_MESSAGE = f"Last: {datetime.datetime.now().time():%H:%M:%S}"
-
-        # Determine message sizes
-        small_text_width, small_height = draw.textsize(TGT_MESSAGE, small_font)
-        big_text_width, big_height = draw.textsize(TGT_MESSAGE, big_font)
-
-        # Draw baby draw
-        draw.text((0, 0), TGT_MESSAGE, font=small_font, fill=(255, 255, 255))
-        draw.text(
-            (0, small_height * 1.5), CUR_MESSAGE, font=real_big_font, fill=cur_color
+        display.update_thermostat_display(
+            low_temp=thermostat.low_temp,
+            high_temp=thermostat.high_temp,
+            local_temp=calibrated_local_temp,
+            cur_temp=cur_temp,
+            status=status,
+            last_update=datetime.datetime.now().time()
         )
-        draw.text(
-            (0, small_height * 2 + big_height * 1),
-            STATUS_MESSAGE,
-            font=real_big_font,
-            fill=stat_color,
-        )
-        draw.text(
-            (0, small_height * 1 + big_height * 3),
-            LAST_MESSAGE,
-            font=small_font,
-            fill=(255, 255, 255),
-        )
-
-        display.display(img)
